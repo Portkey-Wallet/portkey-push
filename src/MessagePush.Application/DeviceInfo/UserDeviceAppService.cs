d49173138ea3084ce2b34d334368f0ce9b9db681
felix-zhaolei <felix@portkey.finance>
2024-03-12 18:38:09 +0800

    modify: add refresh token log.

--- src/MessagePush.Application/DeviceInfo/UserDeviceAppService.cs
+++ src/MessagePush.Application/DeviceInfo/UserDeviceAppService.cs
@@ -8,6 +8,7 @@ using MessagePush.DeviceInfo.Provider;
 using MessagePush.Entities.Es;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Volo.Abp;
 
 namespace MessagePush.DeviceInfo;
@@ -29,6 +30,7 @@ public class UserDeviceAppService : MessagePushBaseService, IUserDeviceAppServic
 
     public async Task ReportDeviceInfoAsync(UserDeviceInfoDto input)
     {
+        Logger.LogInformation("report device info input, data:{data}", JsonConvert.SerializeObject(input));
         var id = DeviceInfoHelper.GetId(input.UserId, input.DeviceId, input.NetworkType.ToString());
         var deviceInfo = ObjectMapper.Map<UserDeviceInfoDto, UserDeviceIndex>(input);
         deviceInfo.Id = id;
@@ -36,6 +38,7 @@ public class UserDeviceAppService : MessagePushBaseService, IUserDeviceAppServic
         deviceInfo.AppStatus = AppStatus.Foreground.ToString();
         deviceInfo.AppId = _httpContextAccessor.HttpContext?.Request.Headers.GetOrDefault(CommonConstant.AppIdKeyName);
 
+        Logger.LogInformation("update index:{index}", JsonConvert.SerializeObject(deviceInfo));
         await _deviceInfoRepository.AddOrUpdateAsync(deviceInfo);
         Logger.LogDebug("report device info, appId: {appId}, id: {id}", deviceInfo.AppId ?? string.Empty, id);
     }
@@ -83,7 +86,7 @@ public class UserDeviceAppService : MessagePushBaseService, IUserDeviceAppServic
         Logger.LogDebug("report switch network, appId: {appId}, id: {id},  status: {status}",
             deviceInfo.AppId ?? string.Empty, id, deviceInfo.AppStatus);
     }
-    
+
     public async Task UpdateUnreadMessageAsync(UnreadMessageDto input)
     {
         var appId = GetFromHeader();
