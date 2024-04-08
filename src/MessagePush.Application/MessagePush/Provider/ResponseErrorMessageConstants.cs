04b3583a1649ba9aa4c63b2f89342a8d7388d8b1
Raymond-portkey <robert_smith@portkey.finance>
2024-04-08 20:09:13 +0800

    Feat: static class

--- src/MessagePush.Application/MessagePush/Provider/ResponseErrorMessageConstants.cs
+++ src/MessagePush.Application/MessagePush/Provider/ResponseErrorMessageConstants.cs
@@ -1,6 +1,6 @@
 namespace MessagePush.MessagePush.Provider;
 
-public class ResponseErrorMessageConstants
+public static class ResponseErrorMessageConstants
 {
     public const string EntityNotFoundErrorMessage = "Requested entity was not found";
     public const string InvalidFcmTokenErrorMessage = "The registration token is not a valid FCM registration token";
