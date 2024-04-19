93a439543037fc3c6ec32d4048c01ab530765d6c
nill66 <sofiat.nill@portkey.finance>
2024-04-19 11:00:42 +0800

    feat: merge conflict

--- src/MessagePush.HttpApi.Host/Program.cs
+++ src/MessagePush.HttpApi.Host/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using MessagePush.Common;
 using MessagePush.ScheduledTasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -14,23 +14,7 @@ public class Program
 {
     public async static Task<int> Main(string[] args)
     {
-        var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .Build();
-        Log.Logger = new LoggerConfiguration()
-#if DEBUG
-            .MinimumLevel.Debug()
-#else
-            .MinimumLevel.Information()
-#endif
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .ReadFrom.Configuration(configuration)
-            
-#if DEBUG
-            .WriteTo.Async(c => c.Console())
-#endif          
-            .CreateLogger();
+        Log.Logger = LogHelper.CreateLogger(LogEventLevel.Debug);
 
         try
         {
